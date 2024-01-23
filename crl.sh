0aeeab2eadff522e8b36f6e506e7a432ad961907
johnny096317 <68207841+johnny096317@users.noreply.github.com>
2024-01-23 15:03:40 +0800

    Add files via upload

---
+++ crl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#取得廢止清冊
+wget https://moica.nat.gov.tw/repository/MOICA/CRL2/complete.crl
+
+#取得卡號
+openssl crl -nameopt utf8 -inform DER -text -in complete.crl | grep "Serial Number" | cut -d " " -f 7 > temp
+awk -v RS= '/----/{next}{gsub(/\n/,"\",\"")}BEGIN{printf "\""}7' temp > temp2
+SN="$(cat temp2)\""
+
+#取得版本號
+version=$(openssl crl -nameopt utf8 -inform DER -text -in complete.crl | awk '/X509v3 CRL Number:/ {getline; gsub(/^[ \(/^[ \t]+|[ \t]+$/, ""); print}')
+printf "{\"version\":\"$version\", \"value\":[$SN]}" > moica-crl.json
+
+rm temp temp2 complete.crl
