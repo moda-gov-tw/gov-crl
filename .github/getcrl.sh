220e92786c82240ebda81f69e5ac7da8ca57765d
johnny096317 <68207841+johnny096317@users.noreply.github.com>
2024-01-23 16:26:26 +0800

    Update getcrl.sh

--- .github/getcrl.sh
+++ .github/getcrl.sh
@@ -11,6 +11,6 @@ SN="$(cat temp2)\""
 version=$(openssl crl -nameopt utf8 -inform DER -text -in complete.crl | awk '/X509v3 CRL Number:/ {getline; gsub(/^[ \(/^[ \t]+|[ \t]+$/, ""); print}')
 printf "{\"version\":\"$version\", \"value\":[$SN]}" > moica-crl.json
 #上次執行時間
-date '+%Y-%m-%d %H:%M.%S' > last_executed.txt
+date '+%Y-%m-%d %H:%M.%S' > .github/last_executed.txt
 
 rm temp temp2 complete.crl
